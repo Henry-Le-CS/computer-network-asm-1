aa2907dfd1bcea1d33d617bde7ba57280d2c0efb
Henry Le <lehieu2077@gmail.com>
2023-12-02 01:54:15 +0700

    feat: add cli and publish file for client

--- client/client.py
+++ client/client.py
@@ -4,9 +4,8 @@ import os
 import sys
 import argparse
 
-
-class MyException(Exception):
-    pass
+from client_helper import parse_client_cmd, MyException
+from pathlib import Path
 class Client(object):
     def __init__(
         self, 
@@ -60,23 +59,58 @@ class Client(object):
         try:            
             # TODO: Fetch hostname from the system, if exists then terminate the program
             message = 'SET_CLIENT_NAME\n' + self.hostname
-            self.server.sendall(message.encode())
+            self.server.send(message.encode())
         except Exception as e:
             print(e)
             self.shutdown()
-            
         
             
     def cli(self):
-        pass
+        while True:
+            try:
+                command = input('> ')
                 
-    def shutdown(self):
+                if command == '':
+                    continue
+                
+                method, payload = parse_client_cmd(command)
+                
+                if(hasattr(self, method) and callable(getattr(self, method))):
+                    getattr(self, method)(payload)
+                
+                
+            except Exception as e:
+                print(e)
+            except BaseException:
+                self.shutdown()
+                
+    def shutdown(self, payload = None):
         print('\nShutting Down...')
-        self.server.close()
+
         try:
             sys.exit(0)
         except SystemExit:
             os._exit(0)
+            
+    def publish_file_info(self, payload):
+        file_path, file_name = payload
+        
+        self.create_folder_if_not_exists(file_path)
+        file_exists = self.check_file_exist(file_path, file_name)
+        
+        if not file_exists:
+            print(f'File {file_name} does not exists.\n')
+            return
+        
+        message = 'PUBLISH_FILE_INFO\n' + file_path + '\n' + file_name
+        self.server.send(message.encode())
+    
+    def create_folder_if_not_exists(self, file_path):
+        Path(file_path).mkdir(parents=True, exist_ok=True)
+        
+    def check_file_exist (self, file_path, file_name):
+        path = file_path + '/' + file_name
+        return Path(path).exists()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- client/client_helper.py
+++ client/client_helper.py
@@ -1,41 +1,46 @@
+import socket
+
 class MyException(Exception):
     pass
 
-def validate_command_length (command: str, length: int):
-    splitted_command = command.split()
+def validate_length (text: str, length: int, delimiter: str = ' '):
+    splitted_text = text.split(delimiter)
     
-    if len(splitted_command) < length:
+    if len(splitted_text) < length:
         raise MyException('Missing arguments')
 
-def switch_client_cmd (command: str):
+def parse_client_cmd (command: str):
+    """
+    Args:
+        command (str): User's command line
+
+    Returns:
+        method: Client's method
+        payload: payload for corresponding method
+    """
+    
     splitted_command = command.split()
     
-    methods = ''
+    method = ''
     payload = None
     
     keyword = splitted_command[0].strip()
 
-    if keyword.lower() == 'ping':
-        validate_command_length(command, 2)
+    if keyword.lower() == 'publish':
+        validate_length(command, 3)
         
-        client_name = splitted_command[1]
+        file_path, file_name = splitted_command[1], splitted_command[2]
         
-        payload = client_name
-        methods = 'ping_client'
+        # Replace backslash with slash, so we can make new folder if they do not exists
+        file_path = file_path.replace('\\', '/')
         
-    elif keyword.lower() == 'discover':
-        client_name = splitted_command[1]
-        
-        payload = client_name
-        methods = 'discover_client'
-
-    elif keyword.lower() == 'list':
-        methods = 'list_client'
-        
+        payload = (file_path, file_name)
+        method = 'publish_file_info'
+                
     elif keyword.lower() == 'exit':
-        methods = 'shutdown'
+        method = 'shutdown'
+
     else:
-        print(keyword.lower())
-        raise MyException('Invalid request.')
+        raise MyException('Invalid command !')
     
-    return methods, payload
+    return method, payload
